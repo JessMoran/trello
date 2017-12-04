45bd27d494130b9d18b8c08c8027ef56d20fce14
Jess Moran <j-es-itza@hotmail.com>
2017-12-04 12:37:53 -0600

    Subiendo DOM

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
+var locationForm = document.body.children[1].children[1];
+document. getElementById("addNewFormat").addEventListener("click", newFormat);
+
+function newFormat() {
+  var divContainer = document.createElement("div");
+    divContainer.className = "container-text";
+    locationForm.appendChild(divContainer);
+
+    var inputContainer = document.createElement ("input");
+      inputContainer.className = "input-container";
+      divContainer.appendChild(inputContainer);
+
+    var btnSend = document.createElement ("button");
+      btnSend.className = "btn-send";
+      divContainer.appendChild(btnSend);
+
+    var btnClose = document.createElement ("button");
+      btnClose.className = "btn-close";
+      divContainer.appendChild(btnClose);
+
+}
+/*
+function saveText() {
+
+
+}
+
+
 <div>
                 <input class="save" type="text" name="" value="Guardar">
                 <a href="#"><i class="fa fa-times" aria-hidden="true"></i></a>
-              </div>
+              </div>*/
